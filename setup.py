179ca8b7434ed463dbfe81ae76bfe77049fde71b
Shivank124 <shivankvishwakarma3@gmail.com>
2024-01-10 17:07:01 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical-Chatbot",
+    version = '0.0.0',
+    author = 'Shivank124',
+    author_email = 'shivankvishwakarma3@gmail.com',
+    packages = find_packages(),
+    install_requires = [] 
+)
